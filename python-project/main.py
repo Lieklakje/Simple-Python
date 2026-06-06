899cb47e0db23e6f27fe0c931ffedeba4f663ebb
Lieklakje <112171@st-maartenscollege.nl>
2026-06-06 15:28:52 +0000

    eerst stap

--- python-project/main.py
+++ python-project/main.py
@@ -1,5 +1,5 @@
 import random
-print(input("Hallo speler. Welkom bij het spel Mastermind. De computer genereert een code van 4 kleuren. De kleuren zijn: Rood (R), Groen (G), Blauw (B), Paars (P), Wit (W), Zwart (Z). Je hebt 10 pogingen om de correcte code te raden. Succes!"))
+print("Hallo speler. Welkom bij het spel Mastermind. De computer genereert een code van 4 kleuren. De kleuren zijn: Rood (R), Groen (G), Blauw (B), Paars (P), Wit (W), Zwart (Z). Je hebt 10 pogingen om de correcte code te raden. Succes!")
 R = "rood"
 G = "groen"
 B = "blauw"
